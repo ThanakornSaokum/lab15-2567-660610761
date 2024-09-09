8ca0c5c00a75219c2f46bf8687d462d461227494
ThanakornSaokum <thanakorn01350@gmail.com>
2024-09-09 15:39:48 +0700

    finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string().min(6, {message: "Password must contain at least 6 characters"}).max(12, {message: "Password must contain at least 12 characters"}),
     confirmPassword: z.string(),
   })
   .refine(
@@ -69,7 +69,20 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
-  );
+  )
+  .refine(
+    //refine allows you check error in your own way
+    //in this example, we check "password" and "confirmPassword" fields
+    (data) => {
+      // if user does not fill confirm password, then it's ok
+      if (data.confirmPassword === data.password) return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
+    }
+  )
+  ;
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -97,7 +110,14 @@ export default function Home() {
 
     if (form.values.plan === "funrun") price = 500;
     //check the rest plans by yourself
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //TIP : check /src/app/libs/runningPlans.js
+    if(form.values.hasCoupon && form.values.coupon === "CMU2023"){
+      price *= 0.7;
+    }
+
 
     //check discount here
 
@@ -187,7 +207,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer year={"2024"} fullName="Thanakon Saokham" studentId="660610761" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
